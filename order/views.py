4d0d9103a93ae2f4a3accc6ba192fc901c3dc80d
louis <dobi1115@gmail.com>
2020-06-18 00:38:29 +0900

    Add OrderList View

--- order/views.py
+++ order/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import FormView
+from django.views.generic import ListView
 from .forms import RegisterForm
+from .models import Order
 # Create your views here.
 
 
@@ -21,3 +23,15 @@ class OrderCreate(FormView):
         })
         # 기존에 생성되는 인자값에 request도 함께 생성해서 전달
         return kw
+
+
+class OrderList(ListView):
+    # 다른사람이 주문한 정보도 볼수 있게 됨. 때문에 직접 queryset을 만듬. -> 세션이 필요. 사용자 정보가 필요하기 때문
+    # model = Order
+    template_name = 'order.html'
+    context_object_name = 'order_list'
+
+    def get_queryset(self, **kwargs):
+        queryset = Order.objects.filter(
+            user__email=self.request.session.get('user'))
+        return queryset
